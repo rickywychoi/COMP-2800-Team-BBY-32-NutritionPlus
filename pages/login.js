cd79be1f8be909154de3205f2dbc833c9163b3eb
Jianming Lu <lujianming000@gmail.com>
2020-04-30 22:08:26 -0700

    Jianming_Lu_Login_CSS

--- pages/login.js
+++ pages/login.js
@@ -57,13 +57,154 @@ class SignInScreen extends React.Component {
   render() {
     if (!this.props.isSignedIn) {
       return (
-        <div className={loginStyles.main}>
-          <h1>Nutrition+</h1>
-          <p>Please sign-in:</p>
+        <div className="main">
+          <h1 className="title" align="center">Nutrition+</h1>
+          <p className="sign" align="center">Sign in</p>
+          <form className="form1">
+          <input className="un " type="text" align="center" placeholder="Username" />
+          <input className="pass" type="password" align="center" placeholder="Password" />
+          <a className="submit" align="center">Sign in</a>
+          </form>
+          <p className="forgot" align="center"><a href="#">Forgot Password?</a></p>         
           <br/>
           <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()}/>
-          <br/>
-          <Link href="/"><a>Back to home</a></Link>
+          
+          <Link href="/"><a className="back" align="center" >Back to home</a></Link>
+
+          <style jsx>{`
+            .main {
+              background-color: #FFFFFF;
+              width: 400px;
+              height: 750px;
+              margin: 1em auto;
+              border-radius: 1.5em;
+              box-shadow: 0px 11px 35px 2px rgba(0, 0, 0, 0.14);
+            }
+
+            .title {
+              padding-top: 40px;
+              color: lightgreen;
+              font-family: 'Ubuntu', sans-serif;
+              font-weight: bold;
+              font-size: 35px;
+            } 
+
+
+          
+            .sign {
+                padding-top: 40px;
+                color: #8C55AA;
+                font-family: 'Ubuntu', sans-serif;
+                font-weight: bold;
+                font-size: 23px;
+            }
+            
+            .un {
+              width: 76%;
+              color: rgb(38, 50, 56);
+              font-weight: 700;
+              font-size: 14px;
+              letter-spacing: 1px;
+              background: rgba(136, 126, 126, 0.04);
+              padding: 10px 20px;
+              border: none;
+              border-radius: 20px;
+              outline: none;
+              box-sizing: border-box;
+              border: 2px solid rgba(0, 0, 0, 0.02);
+              margin-bottom: 50px;
+              margin-left: 46px;
+              text-align: center;
+              margin-bottom: 27px;
+              font-family: 'Ubuntu', sans-serif;
+            }
+            
+            form.form1 {
+              padding-top: 40px;
+            }
+            
+            .pass {
+              width: 76%;
+              color: rgb(38, 50, 56);
+              font-weight: 700;
+              font-size: 14px;
+              letter-spacing: 1px;
+              background: rgba(136, 126, 126, 0.04);
+              padding: 10px 20px;
+              border: none;
+              border-radius: 20px;
+              outline: none;
+              box-sizing: border-box;
+              border: 2px solid rgba(0, 0, 0, 0.02);
+              margin-bottom: 50px;
+              margin-left: 46px;
+              text-align: center;
+              margin-bottom: 27px;
+              font-family: 'Ubuntu', sans-serif;
+            }
+            
+          
+            .un:focus, .pass:focus {
+              border: 2px solid rgba(0, 0, 0, 0.18) !important;
+                
+            }
+            
+            .submit {
+                cursor: pointer;
+                border-radius: 5em;
+                color: #fff;
+                background: linear-gradient(to right, #9C27B0, #E040FB);
+                border: 0;
+                padding-left: 40px;
+                padding-right: 40px;
+                padding-bottom: 10px;
+                padding-top: 10px;
+                font-family: 'Ubuntu', sans-serif;
+                margin: auto;
+                font-size: 13px;
+                box-shadow: 0 0 20px 1px rgba(0, 0, 0, 0.04);
+                display:block;
+                width:12em;             
+            }
+
+            .back {
+              cursor: pointer;
+              border-radius: 5em;
+              color: #fff;
+              background: linear-gradient(to right, #9C27B0, #E040FB);
+              border: 0;
+              padding-left: 40px;
+              padding-right: 40px;
+              padding-bottom: 10px;
+              padding-top: 10px;
+              font-family: 'Ubuntu', sans-serif;
+              margin: auto;
+              font-size: 13px;
+              box-shadow: 0 0 20px 1px rgba(0, 0, 0, 0.04);
+              display:block;
+              width:14em;             
+          }
+
+
+            
+            .forgot {
+                text-shadow: 0px 0px 3px rgba(117, 117, 117, 0.12);
+                color: #E1BEE7;
+                padding-top: 15px;
+            }
+            
+            a {
+                text-shadow: 0px 0px 3px rgba(117, 117, 117, 0.12);
+                color: #E1BEE7;
+                text-decoration: none
+            }
+            
+            @media (max-width: 600px) {
+                .main {
+                    border-radius: 0px;
+                }
+        
+      `}</style>
         </div>
       )
     }
