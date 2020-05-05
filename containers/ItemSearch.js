7e06602ceb47927b62486b53ea290610bdf29f7b
rickywychoi <cchaff0105@gmail.com>
2020-05-05 13:52:05 -0700

    Added search bar

--- containers/ItemSearch.js
+++ containers/ItemSearch.js
@@ -1,7 +1,29 @@
+import searchStyles from '../styles/ItemSearch.module.css'
+import { Form } from 'react-bootstrap'
+import { useState, useEffect } from 'react'
+
+
 const ItemSearch = () => {
+  const [search, setSearch] = useState()
+  useEffect(() => {
+
+  })
+
+  const handleSearchChange = e => {
+    setSearch(e.target.value)
+  }
+
   return (
-    <div>
-      Itemsearch.
+    <div className={searchStyles.body}>
+      <Form>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Control 
+            type="text" 
+            placeholder="Search items..."
+            onChange={handleSearchChange}
+          />
+        </Form.Group>
+      </Form>
     </div>
   )
 }
