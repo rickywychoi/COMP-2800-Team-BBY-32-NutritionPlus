acb6c4b96c5235b55b7642383c6a081dbac94fc6
Brian Seo <brian.ssh725@gmail.com>
2020-05-06 15:40:45 -0700

    menu questionnaire change

---
+++ containers/MenuQuestionnaire.js
@@ -0,0 +1,14 @@
+import { Form, Button } from 'react-bootstrap'
+import menuQuestionnaireStyles from '../styles/menuQuestionnaire.module.css'
+import { useRouter } from 'next/router'
+import { EDAMAM_RECIPE_APP_ID, EDAMAM_RECIPE_APP_KEY } from '../apiKey'
+
+const MenuQuestionnaire = () => {
+    return (
+        <div className={menuQuestionnaireStyles.body}>
+            <h1>Search Bar</h1>
+        </div>
+    )
+}
+
+export default MenuQuestionnaire;
