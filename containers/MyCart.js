271f3e0b9c9410ff67bd4c83f73c18ec2c0d4bb9
rickywychoi <cchaff0105@gmail.com>
2020-05-06 01:47:37 -0700

    Initial Commit on My_Cart

---
+++ containers/MyCart.js
@@ -0,0 +1,9 @@
+const MyCart = () => {
+  return (
+    <div>
+      MyCart
+    </div>
+  )
+}
+
+export default MyCart
---
+++ pages/mycart/index.js
@@ -0,0 +1,11 @@
+import MyCart from '../../containers/MyCart'
+
+const MyCartPage = () => {
+  return (
+    <div>
+      <MyCart />
+    </div>
+  )
+}
+
+export default MyCartPage
