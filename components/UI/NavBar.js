4d137b9fdef37124cf76a7bc70a113fd2ca015c7
Brian Seo <brian.ssh725@gmail.com>
2020-05-07 17:43:51 -0700

    test

--- components/UI/NavBar.js
+++ components/UI/NavBar.js
@@ -29,6 +29,7 @@ const NavBar = (props) => {
         <Navbar bg="dark" variant="dark" className = { n.nav }>
             <Navbar.Brand href="/">Nutrition+</Navbar.Brand>
                 <Nav>
+                    <Nav.Link href = "#table">Nutrition Table</Nav.Link>
                     <Nav.Link href="../questionnaire" >Your Intake</Nav.Link>
                     <Nav.Link href="../search">Nutrition Facts</Nav.Link>
                     <Nav.Link href="../about">About Us</Nav.Link>
