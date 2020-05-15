2ea5036d0ccd1309b0890de61bce80c2ccdd8052
Jianming Lu <lujianming000@gmail.com>
2020-05-15 13:25:24 -0700

    Footer

--- components/UI/Layout.js
+++ components/UI/Layout.js
@@ -4,26 +4,29 @@ import mainHomeStyles from '../../styles/mainHome.module.css'
 
 const Layout = ({ children }) => {
   return (
-    <>
+    
+    <div className={mainHomeStyles.container}>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="icon" href="/favicon2.ico" />
       </Head>
-      {/* <div className={mainHomeStyles.main}> */}
+      <div className={mainHomeStyles.wrap}> 
       <NavBar />
       {children}
-      {/* </div> */}
-      {/* <footer className={mainHomeStyles.footer}>
+      </div>
+      
+      <footer className={mainHomeStyles.footer}>
         Copyright © 2020 BCIT CST Group 32. All rights reserved.
-        <a
+        {/* <a
           href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
           Copyright © 2020 BCIT CST Group 32. All rights reserved.
-        </a>
-      </footer> */}
-    </>
+        </a> */}
+      </footer>
+    </div>
+  
   )
 }
 
